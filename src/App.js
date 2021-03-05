39c3760192f96ee1bc77f30fb2c36472cd5a02a6
Zer02 <abasdeo29@gmail.com>
2021-03-05 00:59:35 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Let's build a Tinder Clone App! 🚀</h1>
+
+      {/* Header */}
+      {/* Tinder Cards */}
+      {/* Buttons below tinder cards */}
+
+      {/* Chats screen */}
+      {/* Individual chat screen */}
+
     </div>
   );
 }
